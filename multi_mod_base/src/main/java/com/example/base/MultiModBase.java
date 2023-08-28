411c866787db962d87b5e480a53f53c3e00f0314
Nerjal Nosk <nosk.nerjal@gmx.com>
2023-08-28 14:08:20 +0200

    Update 0.0.0.4: Apache Maven-valid POM generation

--- multi_mod_base/src/main/java/com/example/base/MultiModBase.java
+++ multi_mod_base/src/main/java/com/example/base/MultiModBase.java
@@ -1,12 +1,38 @@
+/*
+ * Copyright (C) 2023 Nerjal
+ *
+ * This is licensed under the Public Domain (the "License");
+ * you may not give this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://creativecommons.org/publicdomain/zero/1.0/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the license.
+ */
+
 package com.example.base;
 
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * Base initializer class
+ */
 public class MultiModBase implements ModInitializer {
-    public static final Logger LOGGER = LogManager.getLogger();
+    /**
+     * Mod ID string and namespace
+     */
     public static final String MOD_ID = "multi_mod_base";
+
+    /**
+     * Mod logger
+     */
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
--- multi_mod_part/src/main/java/com/example/part/MultiModPart.java
+++ multi_mod_part/src/main/java/com/example/part/MultiModPart.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2023 Nerjal
+ *
+ * This is licensed under the Public Domain (the "License");
+ * you may not give this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://creativecommons.org/publicdomain/zero/1.0/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the license.
+ */
+
 package com.example.part;
 
 import com.example.base.MultiModBase;
